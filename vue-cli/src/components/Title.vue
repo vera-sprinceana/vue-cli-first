f8b3a394701a807bb742535ab847ee11178e5e5b
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-04-12 09:14:46 +0200

    Vue CLI First

---
+++ vue-cli/src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<style scoped>
+h1 {
+  color:brown;
+}
+</style>
